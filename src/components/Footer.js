53eb2c708b5fcddbc04e760fa8bc9b2ad2c0ca98
sconsultancy <148803608+sconsultancy@users.noreply.github.com>
2023-10-24 17:05:27 +0530

    final check

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -12,7 +12,7 @@ function Footer() {
           </h1>
         </div>
         <div>
-          {/* <div className="flex bottom-14 space-x-8">
+          <div className="flex bottom-14 space-x-8">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               className="w-5 fill-[#fff]"
@@ -43,12 +43,12 @@ function Footer() {
                 <path d="M12 2.163c3.204 0 3.584.012 4.85.07 3.252.148 4.771 1.691 4.919 4.919.058 1.265.069 1.645.069 4.849 0 3.205-.012 3.584-.069 4.849-.149 3.225-1.664 4.771-4.919 4.919-1.266.058-1.644.07-4.85.07-3.204 0-3.584-.012-4.849-.07-3.26-.149-4.771-1.699-4.919-4.92-.058-1.265-.07-1.644-.07-4.849 0-3.204.013-3.583.07-4.849.149-3.227 1.664-4.771 4.919-4.919 1.266-.057 1.645-.069 4.849-.069zm0-2.163c-3.259 0-3.667.014-4.947.072-4.358.2-6.78 2.618-6.98 6.98-.059 1.281-.073 1.689-.073 4.948 0 3.259.014 3.668.072 4.948.2 4.358 2.618 6.78 6.98 6.98 1.281.058 1.689.072 4.948.072 3.259 0 3.668-.014 4.948-.072 4.354-.2 6.782-2.618 6.979-6.98.059-1.28.073-1.689.073-4.948 0-3.259-.014-3.667-.072-4.947-.196-4.354-2.617-6.78-6.979-6.98-1.281-.059-1.69-.073-4.949-.073zm0 5.838c-3.403 0-6.162 2.759-6.162 6.162s2.759 6.163 6.162 6.163 6.162-2.759 6.162-6.163c0-3.403-2.759-6.162-6.162-6.162zm0 10.162c-2.209 0-4-1.79-4-4 0-2.209 1.791-4 4-4s4 1.791 4 4c0 2.21-1.791 4-4 4zm6.406-11.845c-.796 0-1.441.645-1.441 1.44s.645 1.44 1.441 1.44c.795 0 1.439-.645 1.439-1.44s-.644-1.44-1.439-1.44z" />
               </svg>
             </span>
-          </div> */}
+          </div>
         </div>
       </div>
 
       <div className="flex mt-16 space-x-10 justify-center ">
-        {/* <div className="flex flex-col justify-center ">
+        <div className="flex flex-col justify-center ">
           <h1 className=" font-medium text-white text-2xl pl-1">SCG</h1>
           <div className=" mt-3 pl-4 flex  justify-between bg-[#2A2F39] rounded-full  ">
             <input
@@ -60,9 +60,9 @@ function Footer() {
               Getting Started
             </button>
           </div>
-        </div> */}
+        </div>
 
-        {/* <div className="flex space-x-8 text-white">
+        <div className="flex space-x-8 text-white">
           <div className="flex flex-col space-y-3">
             <h1 className=" text-xl  font-normal">DESIGN</h1>
             <h3 className="pt-2 opacity-60 text-md">Digital Branding</h3>
@@ -81,15 +81,15 @@ function Footer() {
             <h3 className="opacity-60 text-md">SEO</h3>
             <h3 className="opacity-60 text-md">A-B Testing</h3>
           </div>
-        </div> */}
+        </div>
       </div>
-      {/* <div className="flex w-3/4 justify-between mt-16 text-white opacity-80">
+      <div className="flex w-3/4 justify-between mt-16 text-white opacity-80">
         <div className=" ">SCG</div>
         <div className="flex space-x-5">
           <h3>Terms</h3>
           <h3>Credits</h3>
         </div>
-      </div> */}
+      </div>
     </div>
   );
 }
