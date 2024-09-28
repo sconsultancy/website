9e6662c7c5821fed1f02950bd6281890cc07ded8
sconsultancy <148803608+sconsultancy@users.noreply.github.com>
2024-09-29 00:19:50 +0530

    added nodemailer

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -82,13 +82,7 @@ function Navbar() {
               );
             })}
           </div>
-<<<<<<< HEAD
           {/* <div className="flex items-center">
-=======
-        { /*
-        
-        <div className="flex items-center">
->>>>>>> 7ba55f5a9fe9d56232b7091f823b70030b6a15bc
             <button
               className=" mx-3 h-[51px] w-[143px] rounded-full  border-[1px] border-black font-semibold text-lg"
               onClick={handleLogin}
@@ -107,16 +101,7 @@ function Navbar() {
               isSignup={isSignup}
               switchType={switchType}
             ></Modal>
-<<<<<<< HEAD
           </div> */}
-=======
-          </div>
-        
-        */
-
-          
-        }  
->>>>>>> 7ba55f5a9fe9d56232b7091f823b70030b6a15bc
         </div>
       </div>
       <NavbarDrawer
