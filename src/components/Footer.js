18a83de9d7928f99d99e6f06b0a038cc7516bf15
sconsultancy <148803608+sconsultancy@users.noreply.github.com>
2024-09-29 14:26:47 +0530

    added email and Social Media Links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,21 @@
 "use client";
 
+import axios from "axios";
 import Link from "next/link";
-import React from "react";
+import React, { useState } from "react";
 
 function Footer() {
+  const [input, setInput] = useState("");
+  const handleButtonClick = async () => {
+    const res = await axios.post(
+      "https://render-test-p5hr.onrender.com/api/email",
+      {
+        email: input,
+      }
+    );
+
+    console.log(res);
+  };
   return (
     <div className=" bg-[#232526]    flex-col p-10 pb-5 w-full ">
       <div className="flex max-[581px]:grid max-[581px]:gap-6 w-full  justify-around items-center  mt-8 ">
@@ -11,7 +23,7 @@ function Footer() {
           {`"`}Unleash Your Potential{`"`}
         </h1>
         <div className="flex bottom-14 space-x-8 justify-center">
-          <Link href={"https://www.facebook.com"}>
+          <Link href={"https://www.facebook.com/profile.php?id=61566453275005"}>
             <svg
               xmlns="http://www.w3.org/2000/svg"
               className="w-5 fill-[#fff]  hover:fill-slate-400"
@@ -20,7 +32,7 @@ function Footer() {
               <path d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z" />
             </svg>
           </Link>
-          <Link href={"https://youtube.com"}>
+          <Link href={"https://youtube.com/@sconsultancy?si=-skADh8ppJY3Kq-4"}>
             <svg
               xmlns="http://www.w3.org/2000/svg"
               className="w-5 fill-[#ffffff] hover:fill-slate-400"
@@ -29,7 +41,7 @@ function Footer() {
               <path d="M19.615 3.184c-3.604-.246-11.631-.245-15.23 0-3.897.266-4.356 2.62-4.385 8.816.029 6.185.484 8.549 4.385 8.816 3.6.245 11.626.246 15.23 0 3.897-.266 4.356-2.62 4.385-8.816-.029-6.185-.484-8.549-4.385-8.816zm-10.615 12.816v-8l8 3.993-8 4.007z" />
             </svg>
           </Link>
-          <Link href={"https://twitter.com"}>
+          <Link href={"https://x.com/Sconsultancy_"}>
             <svg
               xmlns="http://www.w3.org/2000/svg"
               className="w-5 fill-[#ffffff] hover:fill-slate-400"
@@ -39,7 +51,7 @@ function Footer() {
             </svg>
           </Link>
 
-          <Link href={"https://www.instagram.com"}>
+          <Link href={"https://www.instagram.com/sconsultancy_/"}>
             <svg
               xmlns="http://www.w3.org/2000/svg"
               className="w-5 h-5 fill-white hover:fill-slate-400"
@@ -58,9 +70,13 @@ function Footer() {
             <input
               type="text"
               placeholder="Your Work Email"
+              onChange={(e) => setInput(e.target.value)}
               className=" bg-black bg-opacity-0 w-full  outline-none  font-semibold text-white text-opacity-40 py-4"
             />
-            <button className="  bg-[#615FEB] text-white rounded-full w-[149px]  h-14 text-sm ">
+            <button
+              onClick={handleButtonClick}
+              className="  bg-[#615FEB] text-white rounded-full w-[149px]  h-14 text-sm "
+            >
               Get Started
             </button>
           </div>
